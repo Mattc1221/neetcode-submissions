61907e729045c9bd088010825f5dacbec8fce5b6
Matthew Chiang <mchiang2112@gmail.com>
2026-08-19 15:26:21 -0500

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,27 @@
+class Solution:
+    def serialize(self, s: str) -> str:
+        arr = [0] * 26
+        for c in s:
+            i = ord(c) - ord("a")
+            arr[i] += 1
+        return str(arr)
+
+
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
+        serialized_s1 = self.serialize(s1)
+        i, j = 0, 0
+        arr = [0] * 26
+        # arr[ord(s2[i]) - ord("a")] = 1
+        while j < len(s2):
+            char_index = ord(s2[j]) - ord("a")
+            arr[char_index] += 1
+            if j - i + 1 == len(s1):
+                if str(arr) == serialized_s1: 
+                    return True
+                arr[ord(s2[i]) - ord("a")] -= 1
+                i += 1
+            j += 1
+        return False
+
+        
