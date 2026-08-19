784b9903902b22badc7538131d2f6d2dfd0fc934
Matthew Chiang <mchiang2112@gmail.com>
2026-08-19 14:00:38 -0500

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.py
@@ -0,0 +1,36 @@
+class Solution:
+    def lastSeenAtDict(self, s: str) -> int:
+        if len(s) == 0: return 0
+
+        max_len = 1
+        i, j = 0, 1
+        last = { s[i]: 0 }
+
+        while j < len(s):
+            if s[j] not in last & last[s[j]] >= i:
+                last[s[j]] = j
+                j += 1
+                max_len = max(max_len, j - i)
+            else:
+                i = last[s[j]] + 1
+
+
+    def shrinkingSoln(self, s: str) -> int:
+        if len(s) <= 1: return len(s)
+
+        max_len = 1
+        i, j = 0, 1
+        seen = { s[0] }
+        while j < len(s):
+            if s[j] not in seen:
+                seen.add(s[j])
+                j += 1
+                max_len = max(len(seen), max_len)
+            else:
+                seen.remove(s[i])
+                i += 1
+        return max_len
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        return self.shrinkingSoln(s)
+
